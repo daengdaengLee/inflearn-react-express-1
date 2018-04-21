2371aa63917bb9406b911c6f24aefff84755225b
Kunho Lee <gunho1020@gmail.com>
2018-04-21 19:33:44 +0900

    ENH: use Contact Component in App Component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import Contact from '../Contact';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        hi react!
-      </div>
+      <Contact />
     );
   }
 }
